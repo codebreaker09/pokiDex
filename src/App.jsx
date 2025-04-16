177e57eaf4899aae1713f80fd6a46052de5c8402
codebreaker09 <sanchezjm109@hotmail.com>
2025-04-15 22:41:14 -0400

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return <h1>PokiDex</h1>;
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=60')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('Pokemon:', data.results);
+      })
+      .catch((error) => {
+        console.error('Error fetching Pokemon:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Pokedex</h1>
+      <p>Filler for now</p>
+    </div>
+  );
 }
 
 export default App;
