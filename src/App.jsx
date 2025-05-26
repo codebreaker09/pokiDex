898a00167519f0d4baab9b5c4b854631a8db99ea
codebreaker09 <sanchezjm109@hotmail.com>
2025-05-26 13:27:26 -0400

    push

--- src/App.jsx
+++ src/App.jsx
@@ -31,10 +31,10 @@ function App() {
       />
       <button onClick={handleSearch}>Search</button>
 
-      {(
+      {pokemon && (
         <div>
           <h2>{pokemon.name}</h2>
-          <img src={pokemon.image} alt={pokemon.name} />
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
           <p>Height: {pokemon.height}</p>
           <p>Weight: {pokemon.weight}</p>
           <p>Type: {pokemon.types.map((t) => t.type.name).join(', ')}</p>
