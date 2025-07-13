787d1b907a497d0d6d106a92f009a6004e6b59a9
Jose Sanchez <sanchezjm109@hotmail.com>
2025-07-13 17:54:40 -0400

    storage

--- src/hooks/useSearch.jsx
+++ src/hooks/useSearch.jsx
@@ -1,36 +1,57 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const useSearch = () => {
   const [search, setSearch] = useState('');
   const [pokemonData, setPokemonData] = useState(null);
   const [description, setDescription] = useState('');
+  const [favorites, setFavorites] = useState([]);
 
   const handleSearch = () => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${search.toLowerCase()}`)
-      .then((res) => res.json())
-      .then((pokemon) => {
-        setPokemonData(pokemon);
-      })
-      .catch((err) => {
-        console.error(err, 'Error');
-      });
-  };
+    .then((res) => res.json())
+    .then((data) => {
+      setPokemonData(data)
+    })
+    .catch((err) => {
+      console.error('Error:', err)
+    });
+  }
 
   useEffect(() => {
-    if (pokemonData?.id) {
+    if(pokemonData?.id){
       fetch(`https://pokeapi.co/api/v2/characteristic/${pokemonData.id}`)
-        .then((res) => res.json())
-        .then((characteristic) => {
-          const english = characteristic.descriptions.find(
-            (desc) => desc.language.name === 'en'
-          );
-          setDescription(english.description);
-        })
-        .catch((err) => {
-          console.error(err, 'Error');
-        });
+      .then((res) => res.json())
+      .then((characteristic) => {
+        const english = characteristic.descriptions.find(
+          (desc) => desc.language.name === 'en'
+        );
+        setDescription(english?.description)
+      })
+      .catch((err) => {
+        console.error('Error:', err)
+      });
     }
   }, [pokemonData]);
+
+  useEffect(() => {
+    const stored = localStorage.getItem('favorites');
+    if (stored) {
+      setFavorites(JSON.parse(stored));
+    }
+  }, []);
+
+  const handleFavorite = () => {
+    if(!pokemonData) return;
+
+    const newFavorite = {
+      name: pokemonData.name,
+      image: pokemonData.sprites.front_default,
+    };
+
+    const updatedFavorites = [...favorites, newFavorite];
+    setFavorites(updatedFavorites);
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+  }
 
   return {
     handleSearch,
@@ -38,6 +59,8 @@ const useSearch = () => {
     setSearch,
     pokemonData,
     description,
+    handleFavorite,
+    favorites,
   };
 };
 
